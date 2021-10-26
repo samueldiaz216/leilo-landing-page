8b52dc43a9c6145da6ecf594ee93cbb4d6e5bf6f
samueldiaz <samueldiaz216@gmail.com>
2021-10-26 19:59:10 -0400

    Fix some of the responsivness, Connected to Mailchimp and another API

--- index.js
+++ index.js
@@ -3,8 +3,62 @@ numberOfCasesValue=document.querySelector("#number-of-cases").value;
 casesIndicator=document.querySelector(".cases");
 decrementCases=document.querySelector(".decrement-cases");
 incrementCases=document.querySelector(".increment-cases");
+subscribeRadioButton=document.querySelector(".subscribe");
+oneTimeOrderRadioButton=document.querySelector(".one-time-order");
+counter=document.querySelector(".variety-pack-content-counter");
+buyVarietyPackForm=document.querySelector(".variety-pack-content form");
 
-casesIndicator.innerText=numberOfCasesValue+" cases";
+
+addToCartButton=document.createElement("button");
+
+addToCartButton.classList.add("add-to-cart-button");
+addToCartButton.classList.add("leilo-button-blue");
+addToCartButton.innerText="add to cart";
+
+buyVarietyPackForm.appendChild(addToCartButton);
+
+
+
+
+
+addSubscriptionToCartAnchorTag=document.createElement("a");
+addSubscriptionToCartAnchorTag.setAttribute("href","https://withfriends.co/Leilo/join?utm_campaign=wf-websitelink");
+subAddToCartButton=document.createElement("button");
+
+subAddToCartButton.classList.add("add-to-cart-button");
+subAddToCartButton.classList.add("leilo-button-blue");
+subAddToCartButton.innerText="add to cart";
+addSubscriptionToCartAnchorTag.appendChild(subAddToCartButton);
+
+subscriptionScript=document.createElement("script");
+subscriptionScript.setAttribute("type", "text/javascript");
+subscriptionScript.setAttribute("src", "https://danjg53usxhfc.cloudfront.net/api/wf-embed-btn.js?b=Leilo");
+
+
+addSubscriptionToCartButtonHTML="<a href='https://withfriends.co/Leilo/join?utm_campaign=wf-websitelink'>"+addToCartButton+"</a><script type='text/javascript' src='https://danjg53usxhfc.cloudfront.net/api/wf-embed-btn.js?b=Leilo'></script>";
+
+
+
+
+oneTimeOrderRadioButton.addEventListener("click",()=>{
+    counter.classList.toggle("active");
+    numberOfCasesValue=1;
+    casesIndicator.innerText="1 case";
+    buyVarietyPackForm.removeChild(addSubscriptionToCartAnchorTag);
+    buyVarietyPackForm.removeChild(subscriptionScript);
+    buyVarietyPackForm.appendChild(addToCartButton);
+})
+
+subscribeRadioButton.addEventListener("click",()=>{
+    counter.classList.toggle("active");
+    numberOfCasesValue=0;
+    casesIndicator.innerText=numberOfCasesValue+" cases";
+    buyVarietyPackForm.removeChild(addToCartButton);
+    buyVarietyPackForm.appendChild(addSubscriptionToCartAnchorTag);
+    buyVarietyPackForm.appendChild(subscriptionScript);
+})
+
+
 
 decrementCases.addEventListener("click",(e)=>{
     e.preventDefault();
@@ -31,6 +85,9 @@ incrementCases.addEventListener("click",(e)=>{
         casesIndicator.innerText=numberOfCasesValue+" cases";
     }
 })
+
+
+
 
 
 

1eb7e250b192300505274d417b29363bb89800f0
samueldiaz <samueldiaz216@gmail.com>
2021-10-30 02:46:37 -0400

    Made many components responsive

--- index.js
+++ index.js
@@ -88,6 +88,41 @@ incrementCases.addEventListener("click",(e)=>{
 
 
 
+//footer code
+
+// var footer = document.getElementById("footer");
+
+// var footerHTML ='<div class="row">\
+//     <div class="logo">\
+//         <img id="footer-logo" src="images/leilo_footer.png" %}">\
+//         <p class="copyright">Made in NYC<br>© Leilo 2020</p>\
+//     </div>\
+//     <div class="links">\
+//         <a href="lemon" class="footer">shop</a>\
+//         <a href="story" class="footer">our story</a>\
+//         <a href="merchcollection" class="footer">merch</a>\
+//         <a href="membership" class="footer">membership</a>\
+//         <a href="contact" class="footer">contact us</a>\
+//         <a href="wholesale" class="footer">wholesale</a>\
+//         <a href="faq" class="footer">faq</a>\
+//         <a href="privacy" class="footer">privacy policy</a>\
+//         <a href="terms" class="footer">terms & conditions</a>\
+//     </div>\
+//     <div class="disclaimer">\
+//         <p class="disclaimer-text">The information and statements on our site have not been evaluated by the FDA. This product is not intended to diagnose, treat, cure, or prevent any disease.</p>\
+//         <p class="disclaimer-text">Not for use by persons under 18 years of age, or by pregnant or breastfeeding women. Do not combine with alcohol, or drive or operate heavy machinery after consumption. US FDA advises that a potential risk of rare, but severe, liver injury may be associated with kava-containing dietary supplements. Ask a healthcare professional before use if you have or have had liver problems or are taking any medication. Stop use and see a doctor if you develop symptoms that may signal liver problems.</p>\
+//     </div>\
+//     <div class="social-media">\
+//         <a href="https://www.instagram.com/drinkleilo/" class="fa fa-instagram"></a>\
+//         <a href="https://www.facebook.com/drinkleilo/" class="fa fa-facebook-square"></a>\
+//         <a href="https://twitter.com/drinkleilo" class="fa fa-twitter"></a>\
+//     </div>\
+// </div>';
+
+// footer.innerHTML = footerHTML;
+
+
+
 
 
 
